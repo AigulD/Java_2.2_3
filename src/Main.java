3db42a7306e7b247e56b9479620a42998113b0f8
AigulD <70228729+AigulD@users.noreply.github.com>
2020-09-28 10:03:53 +0500

    variables were added to Main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        double annualRate = 9.99;
+        double creditTerm1Year = 12;
+        double creditTerm2Years = 24;
+        double creditTerm3Years = 36;
+        double loanAmount = 1_000_000;
+
         CreditPaymentService service = new CreditPaymentService();
 
-        double monthlyLoanPayment = service.calculate(1_000_000, 1,9.99);
-        System.out.printf("При сроке кредита 1 год, ежемесячный платеж: %.0f",monthlyLoanPayment);
+        double monthlyLoanPayment1Y = service.calculate(creditTerm1Year, loanAmount, annualRate);
+        System.out.printf("При сроке кредита 1 год, ежемесячный платеж: %.0f",monthlyLoanPayment1Y);
 
-        monthlyLoanPayment = service.calculate(1_000_000, 2,9.99);
-        System.out.printf("\nПри сроке кредита 2 года, ежемесячный платеж: %.0f",monthlyLoanPayment);
+        double monthlyLoanPayment2Y = service.calculate(creditTerm2Years, loanAmount, annualRate);
+        System.out.printf("\nПри сроке кредита 2 года, ежемесячный платеж: %.0f",monthlyLoanPayment2Y);
 
-        monthlyLoanPayment = service.calculate(1_000_000,3,9.99);
-        System.out.printf("\nПри сроке кредита 3 года, ежемесячный платеж: %.0f",monthlyLoanPayment);
+        double monthlyLoanPayment3Y = service.calculate(creditTerm3Years, loanAmount, annualRate);
+        System.out.printf("\nПри сроке кредита 3 года, ежемесячный платеж: %.0f",monthlyLoanPayment3Y);
     }
 }
